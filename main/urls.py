940ae325eb08dcd6737372a0c72af6a342433099
Jaeyeon Kim <amelia1023@naver.com>
2022-11-30 17:03:44 +0900

    question list by group & number

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,6 @@ router.register(r'question', QuestionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('questionlistbygroup/<str:group>', QuestionListByGroup.as_view()),
+    path('questionlistbynumber/<int:min>/<int:max>', QuestionListByNumber.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -3,8 +3,11 @@ from rest_framework.viewsets import ModelViewSet
 from .models import *
 from .serializers import *
 import os
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+# 질문 CRUD viewset
 class QuestionViewSet(ModelViewSet):
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
@@ -36,3 +39,23 @@ def upload_questions():
     Q = Question.objects.bulk_create(bulk_list)
     print("!!question upload success!!")
     return
+
+# 그룹별(A/B/C)로 질문 리스트 반환
+# ex) 그룹A
+# /api/main/questionlistbygroup/A
+class QuestionListByGroup(APIView):
+    def get(self, request, group):
+        queryset = Question.objects.filter(group=group)
+        serializer_context = {'request': request}
+        serializer_class = QuestionSerializer(queryset, many=True, context=serializer_context)
+        return Response(serializer_class.data)
+
+# 번호 범위대로 질문 리스트 반환
+# ex) 11번 ~ 15번
+# /api/main/questionlistbynumber/11/15
+class QuestionListByNumber(APIView):
+    def get(self, request, min, max):
+        queryset = Question.objects.filter(number__range=[min, max])
+        serializer_context = {'request': request}
+        serializer_class = QuestionSerializer(queryset, many=True, context=serializer_context)
+        return Response(serializer_class.data)
